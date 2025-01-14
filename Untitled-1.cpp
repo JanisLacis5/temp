5306d98bc6851a23756eba673a569fce33b7b759
JanisLacis5 <janislacis06@gmail.com>
2025-01-14 08:50:44 +0200

    init

---
+++ Untitled-1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int IINNTT;
+#define int long long
+#define ff first
+#define ss second
+#define inf INT_MAX
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    
+}
+
+IINNTT main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T = 1; 
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
